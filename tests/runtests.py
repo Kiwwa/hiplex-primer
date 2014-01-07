2b280615cfd4b1b7eb5e357a0b4e60429574c8c7
Luke Shillabeer <lshillabeer@gmail.com>
2014-01-07 12:34:23 +1100

    Created example test and comparison of output

---
+++ tests/runtests.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env/python
+
+import unittest
+import primer_design
+
+def isodd(n):
+	return n % 2 == 1
+
+class isoddTests(unittest.TestCase):
+
+	def testone(self):
+		self.failUnless(isodd(1))
+
+	def testtwo(self):
+		self.failIf(isodd(1))
+
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()

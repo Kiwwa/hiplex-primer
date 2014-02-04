3b86cf97cbf316dcc377cf60964db4452ac6ccb4
Luke Shillabeer <lshillabeer@gmail.com>
2014-02-04 13:07:32 +1100

    Fixed paths for test files

--- tests/testexampleoutput.py
+++ tests/testexampleoutput.py
@@ -1,15 +1,15 @@
 import difflib
 
-print '-' * 80
+print '\n', '-' * 80
 print "Executing compare between generated output log and expected output log..."
 print '-' * 80
 
-example_output = open('../exampledata/output/output.log', 'r').read()
+example_output = open('../exampledata/exampleoutput/output.log', 'r').read()
 static_compare = open('file_compare/static_compare_output.log', 'r').read()
 
-diff2 = difflib.context_diff(example_output.splitlines(), static_compare.splitlines())
+diff2 = list(difflib.context_diff(example_output.splitlines(), static_compare.splitlines()))
 
-if list(diff2) == []:
-	print "No differences between 'generated example output log' and 'expected example output log'."
+if diff2 == []:
+	print "No differences between current output and expected output; this is good.", "\n"
 else:
-	print '\n'.join(list(diff2))
+	print '\n'.join(list(diff2)), "\n"
